eb1132cf0450b6c1f4f425eb7d131c691c8f0c5d
ukyo-hayashi <ukyo.hayashi@gmail.com>
2025-12-28 18:59:37 +0900

    コメントを追加

--- algorithms/utils/rnn.py
+++ algorithms/utils/rnn.py
@@ -1,9 +1,6 @@
 import torch
 import torch.nn as nn
 
-"""RNN modules."""
-
-
 class RNNLayer(nn.Module):
     def __init__(
         self,
@@ -30,7 +27,7 @@ class RNNLayer(nn.Module):
                 else:
                     nn.init.xavier_uniform_(param)
         self.norm = nn.LayerNorm(outputs_dim)
-        # cuDNNのメモリ効率化のためパラメータをフラット化
+        # RNNの重み行列を連続した領域に保存することで高速化可能
         self.rnn.flatten_parameters()
 
     def forward(self, x, hxs, masks):
